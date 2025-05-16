691b17ffebf5fea52c9d11a0dab6780a64c33069
cakmaak <yusuf612844@gmail.com>
2025-05-17 01:18:27 +0300

    Render için WSGI yapılandırması güncellendi

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Add the project directory to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
 python manage.py collectstatic --no-input
 python manage.py migrate 
